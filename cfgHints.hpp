69af776c909be986945a5a9c8e9ceb8d8e213565
nomisum <nomisum@googlemail.com>
2020-12-05 15:59:00 +0100

    checkin before merge

--- cfgHints.hpp
+++ cfgHints.hpp
@@ -1,6 +1,6 @@
 class CfgHints
 {
-    class Engineer
+    class gradSB_Engineer
     {
         // Topic title (displayed only in topic listbox in Field Manual)
         displayName = "Engineer System";
@@ -9,22 +9,22 @@ class CfgHints
             // Hint title, filled by arguments from 'arguments' param
             displayName = "Building Abilities";
             // Optional hint subtitle, filled by arguments from 'arguments' param
-            displayNameShort = "";
+            displayNameShort = "...";
             // Structured text, filled by arguments from 'arguments' param
-             description = "Hello %11! You belong to the engineers team.";
+            description = "Hello %11! You belong to the engineers team. <t size='100'><img image='pics\help_engineers.paa'/></t>";
             // Optional structured text, filled by arguments from 'arguments' param (first argument is %11, see notes bellow), grey color of text
             tip = "Find a description of your abilities in the %3FIELD MANUAL%4 you find in the ESC menu.";
             arguments[] = {
                 "name player" // Simple string will be simply compiled and called, String can also link to localization database in case it starts by str_
             };
             // Optional image
-            image = "pics\help_engineers.paa";
+            image = "pics\logo_tracksntraps.paa";
             // optional parameter for not showing of image in context hint in mission (default false))
             noImage = false;
             // -1 Creates no log in player diary, 0 Creates diary log ( default when not provided )
             // if a dlc's appID Number is used ( see [[getDLCs]] ) and the user does not have the required dlc installed then the advHint will be replaced with
             // configfile >> "CfgHints" >> "DlcMessage" >> "Dlc#"; where # is this properties ( dlc appID ) number
-            dlc = -1;
+            dlc = 0;
         };
     };
 };
