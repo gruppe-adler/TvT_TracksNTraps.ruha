1e42eb2af7183c293606b7abd3e471a6fd8218c3
nomisum <nomisum@gmail.com>
2021-03-28 21:37:41 +0200

    medic settings

--- functions/vehicleRespawn/respawnVariables.hpp
+++ functions/vehicleRespawn/respawnVariables.hpp
@@ -1,3 +1,3 @@
 // Each of these variables will be automatically copied over from old vehicle
 // to new vehicle and are included in the gradTnT_vehicleRespawn event
-#define RESPAWN_VARIABLE_NAMES ["gm_vehicle_attributes", "gradTnT_callsign", "gradTnT_isRepairTank", "gradTnT_vehicleSide", "driver_radio_settings", "gunner_radio_settings", "commander_radio_settings", "gradTnt_bft_type"]
+#define RESPAWN_VARIABLE_NAMES ["gm_vehicle_attributes", "gradTnT_callsign", "gradTnT_isRepairTank", "gradTnT_vehicleSide", "driver_radio_settings", "gunner_radio_settings", "commander_radio_settings", "gradTnt_bft_type", "ace_medical_isMedicalVehicle"]
